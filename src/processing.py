62a8d28e7a2d464aaa91a5ddf59a219419d718a4
fr0st <fr0st1s747@gmail.com>
2024-09-04 12:59:59 +0300

    homework_10_1

--- src/processing.py
+++ src/processing.py
@@ -1,25 +1,17 @@
-from typing import Any
+from typing import List, Dict, Any
 
-list_of_dicts = [
-    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:3 develop
-d filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
-    """
-    Функция принимает на вход список словарей и значение для ключа и возвращает новый
-    список содержащий только те словари у которых ключ содержит переданное в функцию
-    значение.
-    """
-    return [d for d in list_of_dict if d.get("state") == state]
 
-def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
+def filter_by_state(data: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
     """
-    Функция принимает на вход список словарей и значение для ключа и возвращает новый
-    список содержащий только те словари у которых ключ содержит переданное в функцию
-    значение.
+Функция фильтрует данные по указанному состоянию список словарей с данными состояние, по которому необходимо отфильтровать данные (по умолчанию 'EXECUTED')
     """
-    return [d for d in list_of_dict if d.get("state") == state]
+    return [d for d in data if d.get('state') == state]
+
+
+def sort_by_date(date_list: list, reverse_list: bool = True) -> list | bool:
+    """Return filtered list by date"""
+    sorted_list = sorted(date_list, key=lambda date_dict: date_dict.get("date"), reverse=reverse_list)
+    return sorted_list
 
 
 
