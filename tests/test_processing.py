9b39fcff400571e7db29aeacb8c1d1c93f40db37
fr0st <fr0st1s747@gmail.com>
2024-09-04 13:04:27 +0300

    homework_10_1

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -3,8 +3,7 @@ from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.mark.parametrize("n, expected", [([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                           {'id': 939719570, 'state': 'EXECUTED',
-                                            'date': '2018-06-30T02:08:58.425572'}],
+                                           {'id': 939719570, 'state': 'EXECUTED','date': '2018-06-30T02:08:58.425572'}],
                                           [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
                                           ),
@@ -14,8 +13,7 @@ def test_filter_by_state(n, expected):
 
 
 @pytest.mark.parametrize("n, expected", [([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-                                           {'id': 939719570, 'state': 'EXECUTED',
-                                            'date': '2018-06-30T02:08:58.425572'}],
+                                           {'id': 939719570, 'state': 'EXECUTED','date': '2018-06-30T02:08:58.425572'}],
                                           [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
                                            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
                                           ),
