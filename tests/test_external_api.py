47d7688a8a18322218e1487c44818128344e798a
fr0st <fr0st1s747@gmail.com>
2024-09-19 14:41:53 +0300

    homework_12_2

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,42 +1,25 @@
 from unittest.mock import patch
-import pytest
-from src.external_api import currency_conversion
+
+from src.external_api import all_amount_rub_convert
+
+transaction = {
+    "id": 441945886,
+    "state": "EXECUTED",
+    "date": "2019-08-26T10:50:58.294041",
+    "operationAmount": {
+        "amount": "31957.58",
+        "currency": {
+            "name": "руб.",
+            "code": "RUB"
+        }
+    },
+    "description": "Перевод организации",
+    "from": "MasterCard 7158300734726758",
+    "to": "Счет 35383033474447895560"
+}
 
 
-@pytest.fixture
-def trans():
-    return {
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {
-            "amount": "31957.58",
-            "currency": {
-                "name": "���.",
-                "code": "USD"
-            }
-        },
-        "description": "������� �����������",
-        "from": "Maestro 1596837868705199",
-        "to": "���� 64686473678894779589"
-    }
-
-
-@patch('src.external_api.requests.get')
-def test_currency_conversion(mock_get, trans):
-    mock_get.return_value.json.return_value = {
-        'success': True,
-        'query': {
-            'from': 'USD',
-            'to': 'RUB',
-            'amount': 8221.37
-        },
-        'info': {
-            'timestamp': 1724671757,
-            'rate': 91.475458
-        },
-        'date': '2024-08-26',
-        'result': 752053.586137
-    }
-    result = currency_conversion(trans)
-    assert result == 752053.586137
+@patch("requests.get")
+def test_all_amount_rub(mock_get):
+    mock_get.return_value.json.return_value = {'success': True, 'rates': {'RUB': 1.0}}
+    assert all_amount_rub_convert(transaction) == 31957.58
