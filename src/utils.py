34a22630891cdec45e6e34de0c9947991190787b
fr0st <fr0st1s747@gmail.com>
2024-09-22 21:36:28 +0300

    homework_13_2

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
-import json
+import os
 import logging
+import json
+import pandas as pd
+
+log_directory = '../logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
+
+file_handler = logging.FileHandler(os.path.join(log_directory, 'utils.log'), encoding='utf-8')
+logging.basicConfig(level=logging.INFO, handlers=[file_handler])
 
 logger = logging.getLogger('utils')
 file_handler = logging.FileHandler('../logs/utils.log', encoding='utf-8')
@@ -19,7 +28,7 @@ def get_operations_info(filename: str) -> list[dict]:
                 logger.error(f'Ошибка TypeError')
                 return []
     except FileNotFoundError as ex:
-        logger.error(f'Ошибка {ex}]')
+        logger.error(f'Ошибка {ex}')
         return []
     except json.JSONDecodeError as ex:
         logger.error(f'Ошибка JSON: {ex}')
@@ -29,5 +38,43 @@ def get_operations_info(filename: str) -> list[dict]:
         return data_info
 
 
+def get_info_transactions():
+    pass
+
+
+def get_info_transactions_csv(filename: str) -> list[dict]:
+    """ Функция для чтения данных из CSV файла и возвращения их в виде списка словарей.
+    @rtype: object
+    """
+    try:
+        logger.info(f'Чтение данных из файла {filename}')
+        data_info = pd.read_csv(filename).to_dict(orient='records')
+        return data_info
+    except FileNotFoundError as ex:
+        logger.error(f'Ошибка {ex}')
+        return []
+    except Exception as ex:
+        logger.error(f'Ошибка при чтении файла: {ex}')
+        return []
+
+
+def get_info_transactions_xlsx(filename: str) -> list[dict]:
+    """ Функция для чтения данных из XLSX файла и возвращения их в виде списка словарей. """
+    try:
+        logger.info(f'Чтение данных из файла {filename}')
+        data_info = pd.read_excel(filename).to_dict(orient='records')
+        return data_info
+    except FileNotFoundError as ex:
+        logger.error(f'Ошибка {ex}')
+        return []
+    except Exception as ex:
+        logger.error(f'Ошибка при чтении файла: {ex}')
+        return []
+
+
 if __name__ == '__main__':
     print(get_operations_info(filename='../data/operations.json'))
+
+
+    def get_info_transactions():
+        pass
