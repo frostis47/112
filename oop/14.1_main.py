1c7d13b34da1f81bf4ba752c0b15d54be6e5c731
fr0st <fr0st1s747@gmail.com>
2024-10-21 21:38:16 +0300

    homework_14_16

---
+++ oop/14.1_main.py
@@ -0,0 +1,54 @@
+from oop.product import Product
+from category import Category
+if __name__ == "__main__":
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra",  # name
+        "256GB, Серый цвет, 200MP камера",  # description
+        180000.0,  # price
+        5  # quantity
+    )
+    product2 = Product(
+        "Iphone 15",
+        "512GB, Gray space",
+        210000.0,
+        8
+    )
+    product3 = Product(
+        "Xiaomi Redmi Note 11",
+        "1024GB, Синий",
+        31000.0,
+        14
+    )
+
+    category1 = Category(
+        "Смартфоны",
+        "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+        [product1, product2, product3],
+    )
+
+    print(category1.products)
+    product3 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    category1.add_product(product3)
+    print(category1.products)
+    print(category1.product_count)
+
+    new_product = Product.new_product(
+        {
+            "name": "Samsung Galaxy S23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    print(new_product.name)
+    print(new_product.description)
+    print(new_product.price)
+    print(new_product.quantity)
+
+    new_product.price = 800
+    print(new_product.price)
+
+    new_product.price = -100
+    print(new_product.price)
+    new_product.price = 0
+    print(new_product.price)
